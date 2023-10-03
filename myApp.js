f418e4d9054170ac9971c2aa485002a3115a945b
YaroslavTsvirkun <yaroslav.tsvirkun.1992@outlook.com>
2023-10-03 10:09:25 +0300

    Update myApp.js
    change myApp.js, add endpoint /echo

--- myApp.js
+++ myApp.js
@@ -78,3 +78,57 @@ app.get('/now', function(req, res, next) {
 
 
 module.exports = app;
+let path = require('path');
+let express = require('express');
+let app = express();
+
+process.env.MESSAGE_STYLE = "uppercase"
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+var loggerMidleware = (req, res, next) => {
+
+  let ClientIp = req.ip;
+  let path = req.path;
+  let method = req.method;
+
+  console.log(method + " " + path + " - " + ClientIp);
+  next();
+}
+app.use(loggerMidleware);
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get("/json", (req, res) => {
+  let xjson = { "message": "Hello json" };
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    let message = xjson['message'].toUpperCase()
+    xjson['message'] = message;
+    res.json(xjson);
+  } else {
+    res.json(xjson);
+  }
+});
+
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  let time = { "time": req.time }
+  res.json(time);
+})
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+
+
+
+module.exports = app;
